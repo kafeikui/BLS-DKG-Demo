d6afafa05d210bc540377e3c37519c514c1002c1
kafeikui <kkffkk3@protonmail.com>
2022-04-13 16:17:56 +0800

    add solc for the moment

--- crates/dkg-cli/build.rs
+++ crates/dkg-cli/build.rs
@@ -1,15 +1,22 @@
-use std::{fs::File, io::Write};
+use std::{fs::File, io::Write, path::PathBuf};
 
 use ethers::{prelude::Abigen, utils::Solc};
 
-const PATH: &str = "../../solidity/contracts/DKG.sol";
+const CONTRACT_PATH: &str = "../../solidity/contracts/DKG.sol";
+const SOLC_PATH: &str = "../../solidity/bin/solc-linux-amd64-v0.6.6+commit.6c089d02";
+
 // Generates the bindings under `src/`
 fn main() {
     // Only re-run the builder script if the contract changes
-    println!("cargo:rerun-if-changed={}", PATH);
+    println!("cargo:rerun-if-changed={}", CONTRACT_PATH);
+
+    let solc_path: PathBuf = PathBuf::from(SOLC_PATH);
 
     // compile the DKG contract (requires solc on the builder's system)
-    let contracts = Solc::new(PATH).build_raw().expect("could not compile");
+    let contracts = Solc::new(CONTRACT_PATH)
+        .solc_path(solc_path)
+        .build_raw()
+        .expect("could not compile");
     let contract = contracts.get("DKG").expect("contract not found");
 
     let abi = contract.abi.clone();

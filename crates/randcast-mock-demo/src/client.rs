08bf775c832ca6b4135a90095c4f783ee0613da1
kafeikui <kkffkk3@protonmail.com>
2022-04-21 20:03:23 +0800

    feature: add a binary node client and a contract server to demonstrate node rigister and dkg process in randcast

--- crates/randcast-mock-demo/src/client.rs
+++ crates/randcast-mock-demo/src/client.rs
@@ -1,21 +1,82 @@
-use hello_world::greeter_client::GreeterClient;
-use hello_world::HelloRequest;
-
-pub mod hello_world {
-    tonic::include_proto!("helloworld");
-}
+use parking_lot::RwLock;
+use rand::prelude::ThreadRng;
+use randcast_mock_demo::node::cache::{
+    InMemoryBlockInfoCache, InMemoryGroupInfoCache, NodeInfoFetcher,
+};
+use randcast_mock_demo::node::client::ControllerTransactions;
+use randcast_mock_demo::node::monitor::{
+    BlockListener, MockBlockListener, MockStartingGroupingListener, StartingGroupingListener,
+};
+use randcast_mock_demo::node::{cache::InMemoryNodeInfoCache, client::MockControllerClient};
+use std::env;
+use std::sync::Arc;
+use threshold_bls::schemes::bls12_381::G1Scheme;
+use threshold_bls::sig::Scheme;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+    static RNG_FN: fn() -> ThreadRng = rand::thread_rng;
 
-    let request = tonic::Request::new(HelloRequest {
-        name: "Tonic".into(),
+    let args: Vec<String> = env::args().collect();
+
+    let id_address = &args[1];
+
+    println!("id_address: {}", id_address);
+
+    let rng = &mut rand::thread_rng();
+
+    let (private_key, public_key) = G1Scheme::keypair(rng);
+
+    println!("private_key: {}", private_key);
+    println!("public_key: {}", public_key);
+    println!("-------------------------------------------------------");
+
+    let node_cache = InMemoryNodeInfoCache::new(id_address.clone(), private_key, public_key);
+
+    let group_cache = InMemoryGroupInfoCache::new();
+
+    let block_cache = InMemoryBlockInfoCache::new();
+
+    let controller_address = String::from("http://[::1]:50052");
+
+    let mut client =
+        MockControllerClient::new(controller_address, node_cache.get_id_address().to_string())
+            .await?;
+
+    client
+        .node_register(bincode::serialize(&public_key).unwrap())
+        .await?;
+
+    let node_cache_ref = Arc::new(RwLock::new(node_cache));
+
+    let group_cache_ref = Arc::new(RwLock::new(group_cache));
+
+    let block_cache_ref = Arc::new(RwLock::new(block_cache));
+
+    let grouping_listener = MockStartingGroupingListener::new(
+        RNG_FN,
+        block_cache_ref.clone(),
+        node_cache_ref,
+        group_cache_ref,
+    );
+
+    let grouping_listener_task = tokio::spawn(async move {
+        if let Err(e) = grouping_listener.start().await {
+            println!("{:?}", e);
+        };
     });
 
-    let response = client.say_hello(request).await?;
+    let block_listener = MockBlockListener::new(block_cache_ref);
 
-    println!("RESPONSE={:?}", response);
+    let block_listener_task = tokio::spawn(async move {
+        if let Err(e) = block_listener.start().await {
+            println!("{:?}", e);
+        };
+    });
+
+    grouping_listener_task.await?;
+
+    block_listener_task.await?;
 
     Ok(())
 }

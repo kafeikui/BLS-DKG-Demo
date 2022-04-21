42a5b08312ae6a1a9b7b4affa9c0681ac4cb4092
kafeikui <kkffkk3@protonmail.com>
2022-04-21 20:01:08 +0800

    feature: add controller contract mock server: rpc proto and implementation feature: add coordinator contract mock server: rpc proto and implementation

--- crates/randcast-mock-demo/build.rs
+++ crates/randcast-mock-demo/build.rs
@@ -1,5 +1,13 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/helloworld.proto")?;
+    let protos = &["proto/controller.proto", "proto/coordinator.proto"];
+
+    for proto in protos {
+        println!("cargo:rerun-if-changed={}", proto);
+    }
+
+    tonic_build::configure()
+        .out_dir("stub")
+        .compile(protos, &["proto"])?;
 
     Ok(())
 }

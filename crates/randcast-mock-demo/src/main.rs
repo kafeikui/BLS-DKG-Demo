a64988016faf3cd3fa6d5f8556b89a1a20def2f5
kafeikui <kkffkk3@protonmail.com>
2022-04-21 20:04:08 +0800

    improvement: improve the local script demonstrating general randcast workflow

--- crates/randcast-mock-demo/src/main.rs
+++ crates/randcast-mock-demo/src/main.rs
@@ -4,18 +4,20 @@ use dkg_core::primitives::{
     types::DKGOutput,
 };
 use dkg_core::{DKGPhase, Phase2Result};
-use rand::rngs::ThreadRng;
-use randcast_mock_demo::{contract::controller::*, test_helpers::InMemoryBoard};
+use randcast_mock_demo::{
+    contract::controller::*, node::errors::NodeResult, test_helpers::InMemoryBoard,
+};
 use std::collections::HashMap;
 use threshold_bls::{
     curve::bls12381::{self, PairingCurve as BLS12_381},
     group::Curve,
-    poly::Idx,
+    poly::{Eval, Idx},
     sig::{G1Scheme, Scheme, SignatureScheme, ThresholdScheme},
 };
 
+/// This a demo script demonstrating general randcast workflow.
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() -> NodeResult<()> {
     let initial_entropy = 0x8762_4875_6548_6346;
 
     println!(
@@ -46,19 +48,29 @@ async fn main() -> anyhow::Result<()> {
 
     println!("DKG task is emitting...");
 
-    let dkg_task = controller.emit_dkg_task();
+    let dkg_task = controller.emit_dkg_task()?;
 
     let group_index = dkg_task.group_index;
 
     let group_epoch = dkg_task.epoch;
 
     // executes the DKG state machine and ensures that the keys are generated correctly
-    let outputs = run_dkg::<bls12381::Curve, G1Scheme<BLS12_381>>(&mut board, phase0s, rng).await;
+    let outputs = run_dkg::<bls12381::Curve, G1Scheme<BLS12_381>>(&mut board, phase0s).await;
 
     // get the public key (we have already checked that all outputs' pubkeys are the same)
     let public_poly = outputs[0].public.clone();
 
     let pubkey = public_poly.public_key();
+
+    let pp1 = &outputs[1].public;
+    let pp2 = &outputs[2].public;
+
+    let ppp1 = outputs[1].public.eval(2).value;
+    let ppp2 = outputs[2].public.eval(2).value;
+
+    // note they're the same point although print outputs are different
+    println!("{:?}", ppp1);
+    println!("{:?}", ppp2);
 
     println!("DKG result is committing...");
 
@@ -92,7 +104,7 @@ async fn main() -> anyhow::Result<()> {
 
     println!("A signature task is emitting...");
 
-    let signature_task = controller.emit_signature_task();
+    let signature_task = controller.emit_signature_task()?;
 
     let signature_index = signature_task.index;
 
@@ -103,9 +115,22 @@ async fn main() -> anyhow::Result<()> {
         .collect::<Vec<_>>();
 
     // committer verify the partial threshold signatures first
-    partial_sigs.iter().for_each(|partial_sig| {
-        G1Scheme::<BLS12_381>::partial_verify(&public_poly, msg.as_bytes(), partial_sig).unwrap();
-    });
+    partial_sigs
+        .iter()
+        .enumerate()
+        .for_each(|(i, partial_sig)| {
+            G1Scheme::<BLS12_381>::partial_verify(&public_poly, msg.as_bytes(), partial_sig)
+                .unwrap();
+
+            if i == 2 {
+                G1Scheme::<BLS12_381>::partial_verify(pp1, msg.as_bytes(), partial_sig).unwrap();
+                G1Scheme::<BLS12_381>::partial_verify(pp2, msg.as_bytes(), partial_sig).unwrap();
+
+                let partial_2: Eval<Vec<u8>> = bincode::deserialize(partial_sig).unwrap();
+                G1Scheme::<BLS12_381>::verify(&ppp1, msg.as_bytes(), &partial_2.value).unwrap();
+                G1Scheme::<BLS12_381>::verify(&ppp2, msg.as_bytes(), &partial_2.value).unwrap();
+            }
+        });
 
     // then aggregates them
     let sig = G1Scheme::<BLS12_381>::aggregate(t, &partial_sigs).unwrap();
@@ -151,7 +176,6 @@ async fn main() -> anyhow::Result<()> {
 async fn run_dkg<C, S>(
     board: &mut InMemoryBoard<C>,
     phase0s: Vec<joint_feldman::DKG<C>>,
-    rng: &mut ThreadRng,
 ) -> Vec<DKGOutput<C>>
 where
     C: Curve,
@@ -161,7 +185,7 @@ where
     // Phase 1: Publishes shares
     let mut phase1s = Vec::new();
     for phase0 in phase0s {
-        phase1s.push(phase0.run(board, rng).await.unwrap());
+        phase1s.push(phase0.run(board, rand::thread_rng).await.unwrap());
     }
 
     // Get the shares from the board
@@ -189,7 +213,10 @@ where
             Phase2Result::GoToPhase3(_) => unreachable!("should not get here"),
         })
         .collect::<Vec<_>>();
-    assert!(is_all_same(outputs.iter().map(|output| &output.public)));
+    assert!(is_all_same(outputs.iter().map(|output| {
+        // println!("{:?}", output.public);
+        &output.public
+    })));
 
     outputs
 }
@@ -202,7 +229,7 @@ fn setup<C, S, R: rand::RngCore>(
 where
     C: Curve,
     // We need to bind the Curve's Point and Scalars to the Scheme
-    S: Scheme<Public = <C as Curve>::Point, Private = <C as Curve>::Scalar>,
+    S: Scheme<Public = C::Point, Private = <C as Curve>::Scalar>,
 {
     // generate a keypair per participant
     let keypairs = (0..n).map(|_| S::keypair(rng)).collect::<Vec<_>>();
